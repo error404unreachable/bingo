19a25c58782f774ca6b4b8a764d1cead3b7c2522
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-11-16 11:50:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+function fillInputs() {
+ let B1 = prompt("B-1:");
+  if (B1 > 0 && B1 < 16) {
+  document.getElementById("b1").innerHTML = B1;
+  } else {
+    alert("Invalid number...");
+    }
+  let I1 = prompt("I-1:");
+  if (I1 > 0 && I1 < 16) {
+  document.getElementById("i1").innerHTML = I1;
+  } else {
+    alert("Invalid number...");
+    }
+}
