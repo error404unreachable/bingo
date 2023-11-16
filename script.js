b11e5598de918069958e8d363fea8fa5e1d269bc
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-11-16 12:34:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,52 @@ function fillInputs() {
   } else {
     alert("Invalid number...");
     }
+ let N1 = prompt("N-1:");
+  if (N1 > 0 && N1 < 16) {
+  document.getElementById("b1").innerHTML = N1;
+  } else {
+    alert("Invalid number...");
+    }
+ let G1 = prompt("G-1:");
+  if (G1 > 0 && G1 < 16) {
+  document.getElementById("b1").innerHTML = G1;
+  } else {
+    alert("Invalid number...");
+    }
+ let I1 = prompt("I-1:");
+  if (B1 > 0 && B1 < 16) {
+  document.getElementById("b1").innerHTML = B1;
+  } else {
+    alert("Invalid number...");
+    }
+ let B1 = prompt("B-1:");
+  if (B1 > 0 && B1 < 16) {
+  document.getElementById("b1").innerHTML = B1;
+  } else {
+    alert("Invalid number...");
+    }
+ let B1 = prompt("B-1:");
+  if (B1 > 0 && B1 < 16) {
+  document.getElementById("b1").innerHTML = B1;
+  } else {
+    alert("Invalid number...");
+    }
+ let B1 = prompt("B-1:");
+  if (B1 > 0 && B1 < 16) {
+  document.getElementById("b1").innerHTML = B1;
+  } else {
+    alert("Invalid number...");
+    }
+ let B1 = prompt("B-1:");
+  if (B1 > 0 && B1 < 16) {
+  document.getElementById("b1").innerHTML = B1;
+  } else {
+    alert("Invalid number...");
+    }
+ let B1 = prompt("B-1:");
+  if (B1 > 0 && B1 < 16) {
+  document.getElementById("b1").innerHTML = B1;
+  } else {
+    alert("Invalid number...");
+    }
 }
