b09bdc3c73573c5bc41f6ed38e944f85c70a80c3
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-11-17 11:17:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,180 +1,53 @@
+let b1 = document.getElementById("b1");
+let b2 = document.getElementById("b2");
+let b3 = document.getElementById("b3");
+let b4 = document.getElementById("b4");
+let b5 = document.getElementById("b5");
+let i1 = document.getElementById("i1");
+let i2 = document.getElementById("i2");
+let i3 = document.getElementById("i3");
+let i4 = document.getElementById("i4");
+let i5 = document.getElementById("i5");
+let n1 = document.getElementById("n1");
+let n2 = document.getElementById("n2");
+let n3 = document.getElementById("n3");
+let n4 = document.getElementById("n4");
+let n5 = document.getElementById("n5");
+let g1 = document.getElementById("g1");
+let g2 = document.getElementById("g2");
+let g3 = document.getElementById("g3");
+let g4 = document.getElementById("g4");
+let g5 = document.getElementById("g5");
+let o1 = document.getElementById("o1");
+let o2 = document.getElementById("o2");
+let o3 = document.getElementById("o3");
+let o4 = document.getElementById("o4");
+let o5 = document.getElementById("o5");
+
 function fillInputs() {
- let B1 = prompt("B-1:");
-  if (B1 > 0 && B1 < 16) {
-  document.getElementById("b1").innerHTML = B1;
-  } else {
-    alert("Invalid number...");
-    }
-}
- /*
- let B2 = prompt("B-2:");
- if (B2 > 0 && B2 < 16) {
- document.getElementById("b2").innerHTML = B2;
- } else {
-   alert("Invalid number...");
-   }
- let B3 = prompt("B-3:");
- if (B3 > 0 && B3 < 16) {
- document.getElementById("b3").innerHTML = B3;
- } else {
-   alert("Invalid number...");
-   }
-
- let B4 = prompt("B-4:");
- if (B4 > 0 && B4 < 16) {
- document.getElementById("b4").innerHTML = B4;
- } else {
-   alert("Invalid number...");
-   }
-
- let B5 = prompt("B-5:");
- if (B5 > 0 && B5 < 16) {
- document.getElementById("b5").innerHTML = B5;
- } else {
-   alert("Invalid number...");
-   }
- 
-  let I1 = prompt("I-1:");
-  if (I1 > 0 && I1 < 16) {
-  document.getElementById("i1").innerHTML = I1;
-  } else {
-    alert("Invalid number...");
-    }
-
- let I2 = prompt("I-2:");
-  if (I2 > 0 && I2 < 16) {
-  document.getElementById("i2").innerHTML = I2;
-  } else {
-    alert("Invalid number...");
-    }
-
- let I3 = prompt("I-3:");
-  if (I3 > 0 && I3 < 16) {
-  document.getElementById("i3").innerHTML = I3;
-  } else {
-    alert("Invalid number...");
-    }
-
- let I4 = prompt("I-4:");
-  if (I4 > 0 && I4 < 16) {
-  document.getElementById("i4").innerHTML = I4;
-  } else {
-    alert("Invalid number...");
-    }
-
- let I5 = prompt("I-5:");
-  if (I5 > 0 && I5 < 16) {
-  document.getElementById("i5").innerHTML = I5;
-  } else {
-    alert("Invalid number...");
-    }
- 
- let N1 = prompt("N-1:");
-  if (N1 > 0 && N1 < 16) {
-  document.getElementById("n1").innerHTML = N1;
-  } else {
-    alert("Invalid number...");
-    }
-
- let N2 = prompt("N-2:");
-  if (N2 > 0 && N2 < 16) {
-  document.getElementById("n2").innerHTML = N2;
-  } else {
-    alert("Invalid number...");
-    }
-
- let N3 = prompt("N-3:");
-  if (N3 > 0 && N3 < 16) {
-  document.getElementById("n3").innerHTML = N3;
-  } else {
-    alert("Invalid number...");
-    }
-
- let N4 = prompt("N-4:");
-  if (N4 > 0 && N4 < 16) {
-  document.getElementById("n4").innerHTML = N4;
-  } else {
-    alert("Invalid number...");
-    }
-
- let N5 = prompt("N-5:");
-  if (N5 > 0 && N5 < 16) {
-  document.getElementById("n5").innerHTML = N5;
-  } else {
-    alert("Invalid number...");
-    }
- 
- let G1 = prompt("G-1:");
-  if (G1 > 0 && G1 < 16) {
-  document.getElementById("g1").innerHTML = G1;
-  } else {
-    alert("Invalid number...");
-    }
-
- let G2 = prompt("G-2:");
-  if (G2 > 0 && G2 < 16) {
-  document.getElementById("g2").innerHTML = G2;
-  } else {
-    alert("Invalid number...");
-    }
-
- let G3 = prompt("G-3:");
-  if (G3 > 0 && G3 < 16) {
-  document.getElementById("g3").innerHTML = G3;
-  } else {
-    alert("Invalid number...");
-    }
-
- let G4 = prompt("G-4:");
-  if (G4 > 0 && G4 < 16) {
-  document.getElementById("g4").innerHTML = G4;
-  } else {
-    alert("Invalid number...");
-    }
-
- let G5 = prompt("G-5:");
-  if (G5 > 0 && G5 < 16) {
-  document.getElementById("g5").innerHTML = G5;
-  } else {
-    alert("Invalid number...");
-    }
- 
- let I1 = prompt("I-1:");
-  if (I1 > 0 && I1 < 16) {
-  document.getElementById("i1").innerHTML = I1;
-  } else {
-    alert("Invalid number...");
-    }
-}
-
-let I2 = prompt("I-2:");
-  if (I2 > 0 && I2 < 16) {
-  document.getElementById("i2").innerHTML = I2;
-  } else {
-    alert("Invalid number...");
-    }
-}
-
-let I3 = prompt("I-3:");
-  if (I3 > 0 && I3 < 16) {
-  document.getElementById("i3").innerHTML = I3;
-  } else {
-    alert("Invalid number...");
-    }
-}
-
-let I4 = prompt("I-4:");
-  if (I4 > 0 && I4 < 16) {
-  document.getElementById("i4").innerHTML = I4;
-  } else {
-    alert("Invalid number...");
-    }
-}
-
-let I5 = prompt("I-5:");
-  if (I5 > 0 && I5 < 16) {
-  document.getElementById("i5").innerHTML = I5;
-  } else {
-    alert("Invalid number...");
-    }*/
+ b1.innerHTML = "";
+ b2.innerHTML = "";
+ b3.innerHTML = "";
+ b4.innerHTML = "";
+ b5.innerHTML = "";
+ i1.innerHTML = "";
+ i2.innerHTML = "";
+ i3.innerHTML = "";
+ i4.innerHTML = "";
+ i5.innerHTML = "";
+ n1.innerHTML = "";
+ n2.innerHTML = "";
+ n3.innerHTML = "";
+ n4.innerHTML = "";
+ n5.innerHTML = "";
+ g1.innerHTML = "";
+ g2.innerHTML = "";
+ g3.innerHTML = "";
+ g4.innerHTML = "";
+ g5.innerHTML = "";
+ o1.innerHTML = "";
+ o2.innerHTML = "";
+ o3.innerHTML = "";
+ o4.innerHTML = "";
+ o5.innerHTML = "";
 }
